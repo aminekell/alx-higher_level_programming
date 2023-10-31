f9cfc72be84f12a03bff99f0f057e41ce13dbef1
aminekell <aminekella2003@gmail.com>
2023-10-31 05:00:25 +0100

    done

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+# Write the text to stderr
+sys.stderr.write("that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Exit the script with the status code 1
+sys.exit(1)
+
