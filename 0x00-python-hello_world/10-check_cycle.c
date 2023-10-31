24a321af63d7925c69ca2107eb04eead2ed67bbb
aminekell <aminekella2003@gmail.com>
2023-10-31 05:34:03 +0100

    done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,6 +7,7 @@
  * Return: 1 if cyclical, 0 otherwise
  */
 int check_cycle(listint_t *list)
+{
 	listint_t *slow = list, *fast = list;
 
 	while (fast && fast->next)
@@ -19,3 +20,4 @@ if (slow ==  fast)
 return (1);
 }
 return(0);
+}
